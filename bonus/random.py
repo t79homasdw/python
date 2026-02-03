c8267264586dd05d672e1ef65dd1918e2289c84c
t79homasdw <dan.thomas@3cloudsolutions.com>
2026-02-03 13:21:59 -0500

    add try / except for random.py

--- bonus/random.py
+++ bonus/random.py
@@ -1,9 +1,14 @@
 import random
 
 while True:
-    lower = int(input("Enter lower bound: "))
-    upper = int(input("Enter upper bound: "))
     try:
-        print(f"Random number between {lower} and {upper} is {random.randint(lower, upper)}")
+        lower = int(input("Enter lower bound: "))
+        upper = int(input("Enter upper bound: "))
+        if lower == upper:
+            print("Lower bound must be less than upper bound.")
+        elif lower > upper:
+            print("Lower bound must be less than upper bound.")
+        else:
+            print(f"Random number between {lower} and {upper} is {random.randint(lower, upper)}")
     except ValueError:
-        print("Invalid input. Please enter valid integers.\n  Note: that the lower bound must be less than the upper bound.")
+        print("Invalid input. Please enter valid integers.\nNote: that the lower bound must be less than the upper bound.")
